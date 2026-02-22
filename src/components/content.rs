707e2c45b2431a9337043e0ba0cfe83b6e53feeb
Groumphh <groumphh38@gmail.com>
2026-02-22 15:57:12 +0100

    Updated text

--- src/components/content.rs
+++ src/components/content.rs
@@ -1,6 +1,5 @@
 use yew::prelude::*;
 
-
 #[derive(Properties, PartialEq)]
 pub struct Properties {
     pub selected_page: u8,
@@ -13,11 +12,11 @@ pub fn content(properties: &Properties) -> Html {
         0 => html! {
             <div class="whoami unpressed">
                 if properties.lang == 0 {
-                    <p>{"My name is Benjamin Gallay, and I am currently starting my studies in the Grande Ecole Centrale Supélec, in order to study computer science."}</p>
+                    <p>{"My name is Benjamin Gallay, and I am currently starting my studies in the Grande Ecole Centrale Supélec to study computer science."}</p>
                 } else {
                     <p>{"Je m'appelle Benjamin Gallay, et je suis commence mes études au sein de la Grande Ecole de Centrale Supélec pour y étudier l'informatique."}</p>
                 }
-                
+
                 <p>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}</p>
             </div>
         },
@@ -44,36 +43,36 @@ pub fn content(properties: &Properties) -> Html {
                             <p>{"option informatique"}</p>
                         </li>
                     }
-                    
+
                 </ul>
             </div>
         },
         2 => html! {
             <div class="projects">
-            <a class="project" tabindex="0" href="https://www.archwiki.org">
+            <a class="project" tabindex="0" href="https://github.com/BenjaminGallay/Personal-page">
                 <h3>{"Stupid Project"}</h3>
-                <p>{"this is a short description of an awkward project that no one is ever gonna read or use, and that is soon gonna fall in a pit of uncaring oblivion and embarrassement"}</p>
+                <p>{"this is a short description of a Rust project experimenting with front-end web apps directly compiled from Rust"}</p>
             </a>
-            <a class="project" tabindex="0" href="https://www.archwiki.org">
+            <a class="project" tabindex="0" href="https://github.com/BenjaminGallay/Personal-page">
                 <h3>{"Stupid Project"}</h3>
-                <p>{"this is a short description of an awkward project that no one is ever gonna read or use, and that is soon gonna fall in a pit of uncaring oblivion and embarrassement"}</p>
+                <p>{"this is a short description of a Rust project experimenting with front-end web apps directly compiled from Rust"}</p>
             </a>
-            <a class="project" tabindex="0" href="https://www.archwiki.org">
+            <a class="project" tabindex="0" href="https://github.com/BenjaminGallay/Personal-page">
                 <h3>{"Stupid Project"}</h3>
-                <p>{"this is a short description of an awkward project that no one is ever gonna read or use, and that is soon gonna fall in a pit of uncaring oblivion and embarrassement"}</p>
+                <p>{"this is a short description of a Rust project experimenting with front-end web apps directly compiled from Rust"}</p>
             </a>
-            <a class="project" tabindex="0" href="https://www.archwiki.org">
+            <a class="project" tabindex="0" href="https://github.com/BenjaminGallay/Personal-page">
                 <h3>{"Stupid Project"}</h3>
-                <p>{"this is a short description of an awkward project that no one is ever gonna read or use, and that is soon gonna fall in a pit of uncaring oblivion and embarrassement"}</p>
+                <p>{"this is a short description of a Rust project experimenting with front-end web apps directly compiled from Rust"}</p>
             </a>
-            <a class="project" tabindex="0" href="https://www.archwiki.org">
+            <a class="project" tabindex="0" href="https://github.com/BenjaminGallay/Personal-page">
                 <h3>{"Stupid Project"}</h3>
-                <p>{"this is a short description of an awkward project that no one is ever gonna read or use, and that is soon gonna fall in a pit of uncaring oblivion and embarrassement"}</p>
+                <p>{"this is a short description of a Rust project experimenting with front-end web apps directly compiled from Rust"}</p>
             </a>
         </div>
         },
         _ => html! {
             <h1>{"Error"}</h1>
-        }
+        },
     }
 }
